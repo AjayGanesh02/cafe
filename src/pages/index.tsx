05eb5f3da1b67ce894899b7f6b044922246e574a
Ajay Ganesh <ajganesh2002@gmail.com>
2024-03-24 00:44:38 -0400

    add submitted state on frontend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import axios, { formToJSON } from "axios";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 
 export default function Home() {
+  const [submitted, setSubmitted] = useState(false);
+
   async function onSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
@@ -11,6 +13,9 @@ export default function Home() {
       method: "post",
       data: formToJSON(formData),
     });
+
+    setSubmitted(true);
+    setTimeout(() => setSubmitted(false), 10000);
   }
 
   return (
@@ -18,65 +23,79 @@ export default function Home() {
       <div className="my-8"></div>
       <h1 className="text-5xl">Ingalls Cafe</h1>
       <div className="my-10"></div>
-      <form onSubmit={onSubmit} className="flex flex-col">
-        <div className="grid grid-cols-2">
-          <label htmlFor="drinkChoice">Drink Choice:</label>
-          <div>
+      {submitted ? (
+        <h1>Your order has been submitted!</h1>
+      ) : (
+        <form onSubmit={onSubmit} className="flex flex-col">
+          <div className="grid grid-cols-2">
+            <label htmlFor="drinkChoice">Drink Choice:</label>
             <div>
-              <input
-                type="radio"
-                id="espresso"
-                name="drinkChoice"
-                value="espresso"
-              />
-              <label htmlFor="espresso">Espresso</label>
+              <div>
+                <input
+                  type="radio"
+                  id="espresso"
+                  name="drinkChoice"
+                  value="espresso"
+                />
+                <label htmlFor="espresso">Espresso</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  id="latte"
+                  name="drinkChoice"
+                  value="latte"
+                />
+                <label htmlFor="latte">Cafe Latte</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  id="mocha"
+                  name="drinkChoice"
+                  value="mocha"
+                />
+                <label htmlFor="mocha">Cafe Mocha</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  id="americano"
+                  name="drinkChoice"
+                  value="americano"
+                />
+                <label htmlFor="americano">Americano</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  id="affogato"
+                  name="drinkChoice"
+                  value="affogato"
+                />
+                <label htmlFor="affogato">Affogato</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  id="matcha"
+                  name="drinkChoice"
+                  value="matcha"
+                />
+                <label htmlFor="matcha">Matcha Latte</label>
+              </div>
             </div>
-            <div>
-              <input type="radio" id="latte" name="drinkChoice" value="latte" />
-              <label htmlFor="latte">Cafe Latte</label>
-            </div>
-            <div>
-              <input type="radio" id="mocha" name="drinkChoice" value="mocha" />
-              <label htmlFor="mocha">Cafe Mocha</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                id="americano"
-                name="drinkChoice"
-                value="americano"
-              />
-              <label htmlFor="americano">Americano</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                id="affogato"
-                name="drinkChoice"
-                value="affogato"
-              />
-              <label htmlFor="affogato">Affogato</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                id="matcha"
-                name="drinkChoice"
-                value="matcha"
-              />
-              <label htmlFor="matcha">Matcha Latte</label>
-            </div>
+            <label htmlFor="name">Your Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="rounded-md border border-black"
+            />
           </div>
-          <label htmlFor="name">Your Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="rounded-md border border-black"
-          />
-        </div>
-        <input type="submit" />
-      </form>
+          <input type="submit" />
+        </form>
+      )}
     </main>
   );
 }
